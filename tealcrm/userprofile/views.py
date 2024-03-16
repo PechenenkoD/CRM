679229b51e3b8ae8a209d2cde865fcab338cf249
d.pechenenko <pechenenkod@gmail.com>
2024-03-16 16:24:48 +0100

    work in progress

--- tealcrm/userprofile/views.py
+++ tealcrm/userprofile/views.py
@@ -7,8 +7,10 @@ from .models import Userprofile
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+
         if form.is_valid():
             user = form.save()
+
             Userprofile.objects.create(user=user)
 
             return redirect('/log-in/')
